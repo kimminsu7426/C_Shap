f0cb3f7a92dd8c17a472b65fb8ac22f167d0587b
kimminsu7426 <ms_7426@naver.com>
2020-07-03 17:01:55 +0900

    7월 3일 금요일 C# 공부

---
+++ StudyCShap/IntegralType/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace IntegralType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            sbyte a = sbyte.MaxValue; // sbyte -128 ~ 128
+            byte b = byte.MinValue; // byte 0 ~ 255
+
+            short c = short.MinValue; 
+            ushort d = ushort.MaxValue; 
+
+            int e = int.MaxValue;
+            uint f = uint.MaxValue;
+
+            long g = long.MaxValue;
+            ulong h = ulong.MaxValue;
+
+            ulong i = 20_000_000_000;
+            Console.WriteLine(i); 
+
+            byte a = 255;
+            WriteLine($"a={a}");
+            byte b = 0b1111_1111;
+            WriteLine($"b={b}");
+            byte c = 0xFF;
+            WriteLine($"c={c}");
+
+            byte d = byte.MaxValue;
+            d += 1;
+            WriteLine($"d={d}");
+            d += 1;
+            WriteLine($"d={d}");
+
+            float f = float.MaxValue;
+            double g = double.MaxValue; 
+
+        }
+    }
+}
---
+++ StudyCShap/StringNumberConversion/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace StringNumberConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int FIXVAL = 20;
+
+            object trans = FIXVAL;
+            
+
+
+
+
+            int a = 12345;
+            string b = a.ToString(); // 문자열b = 12345가 들어가있음
+            Console.WriteLine($"b={b}");
+
+            float c = 3.141592f;
+            string d = c.ToString();
+            Console.WriteLine($"d={d}");
+
+            string g = "3.141592";
+            float h;
+            if (float.TryParse(g, out h))
+            {
+                Console.WriteLine($"h = {h}");
+            }
+            else
+            {
+                Console.WriteLine("g형 변환시 에러발생, 문자열 확인요망");
+            }
+            // bool 논리형식 True, False로 표현됌
+        }
+    }
+}
